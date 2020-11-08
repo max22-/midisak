115928a663f3fa385058c46f4488f3d156bbf238
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2020-11-08 17:28:26 +0100

    Renamed some variables

--- send.cpp
+++ send.cpp
@@ -6,26 +6,23 @@
 
 using namespace std;
 
-void send(RtMidiOut *midiOut, const unsigned char command, const unsigned char param1, const unsigned char param2, const unsigned char param3) {
+void send(RtMidiOut *midiOut, const unsigned char command, const unsigned char channel, const unsigned char param1, const unsigned char param2) {
     vector<unsigned char> message;
 
     if(midiOut == nullptr)
         throw runtime_error("send() : midiOut == nullptr");
 
-    const unsigned char channel = param1;
     if(channel > 15)
         throw runtime_error("send() : invalid channel");
 
     message.push_back(command | channel);
-    message.push_back(param2);
-    if(param3 < 128)
-        message.push_back(param3);
-    for(auto e: message)
-        cout << (int) e << endl;
+    message.push_back(param1);
+    if(param2 < 128)
+        message.push_back(param2);
     midiOut->sendMessage(&message);
 }
 
-void send(const string portName, const string command, const string param1, const string param2, const string param3)
+void send(const string portName, const string command, const string channel, const string param1, const string param2)
 {
     RtMidiOut *midiOut = nullptr;
 
@@ -39,17 +36,17 @@ void send(const string portName, const string command, const string param1, cons
         exit(EXIT_FAILURE);
     }
 
-    const unsigned char channel = std::stoi(param1) - 1;
-    const unsigned char byte2 = stoi(param2), byte3 = stoi(param3);
+    const unsigned char ichannel = std::stoi(channel) - 1;
+    const unsigned char byte2 = stoi(param1), byte3 = stoi(param2);
 
     if(command == "noteon")
-        send(midiOut, NOTEON, channel, byte2, byte3);
+        send(midiOut, NOTEON, ichannel, byte2, byte3);
     else if(command == "noteoff")
-        send(midiOut, NOTEOFF, channel, byte2, byte3);  
+        send(midiOut, NOTEOFF, ichannel, byte2, byte3);  
     else if(command == "cc")
-        send(midiOut, CONTROL_CHANGE, channel, byte2, byte3);
+        send(midiOut, CONTROL_CHANGE, ichannel, byte2, byte3);
     else if(command == "pc")
-        send(midiOut, PROGRAM_CHANGE, channel, byte2);      
+        send(midiOut, PROGRAM_CHANGE, ichannel, byte2);      
     else {
         cerr << command << " : unknown command." << endl;
         delete midiOut;
--- send.h
+++ send.h
@@ -6,16 +6,16 @@
 void send(
     RtMidiOut *midiOut, 
     const unsigned char command, 
+    const unsigned char channel, 
     const unsigned char param1, 
-    const unsigned char param2, 
-    const unsigned char param3 = 255);
+    const unsigned char param2 = 255);
 
 void send(
     const std::string portName,
     const std::string command,
+    const std::string channel,
     const std::string param1,
-    const std::string param2,
-    const std::string param3 = "");
+    const std::string param2 = "");
 
 void send(const std::string portName);    // send commands from stdin
 
